83bed056b13681e57e7cdb6abe0f1e750894bb96
Dhiya <adiadiadha@google.com>
2023-11-14 06:03:11 +0700

    new dependencies part 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,21 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Ui, Cardview Android
+    implementation("androidx.cardview:cardview:1.0.0")
+
+    // retrofit version
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    // GSON
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    // coroutine
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+
+    //    Github Bumbtech
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+
+    // effect shimer
+    implementation ("com.facebook.shimmer:shimmer:0.5.0")
 }
